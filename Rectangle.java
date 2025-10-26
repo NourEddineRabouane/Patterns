3e682a508e0220e9d3d9111300e857a1c0035fd6
NourEddineRabouane <rabouanenoureddine1@gmail.com>
2025-10-26 14:30:11 +0100

    Created Rectangle Pattern

---
+++ Rectangle.java
@@ -0,0 +1,32 @@
+package patterns;
+import java.util.Scanner;
+
+/*
+ *   ******************
+ *   ******************
+ *   ******************
+ *   ******************
+ */
+
+public class Rectangle {
+    public static void main(String[] args) throws Exception {
+        
+        int rows , cols;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Rows number : ");
+        rows = scanner.nextInt();
+
+        System.out.print("Cols number : ");
+        cols = scanner.nextInt();
+
+        for ( int i = 1 ; i <= rows ; i++) {
+            for ( int j = 1 ; j <= cols ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}

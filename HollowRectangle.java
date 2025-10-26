2e1b6fc067003b117762f98fd4f95e2dc09df9b1
NourEddineRabouane <rabouanenoureddine1@gmail.com>
2025-10-26 15:40:16 +0100

    Made the Hollow Rectangle code

---
+++ HollowRectangle.java
@@ -0,0 +1,39 @@
+package patterns;
+import java.util.Scanner;
+
+/*
+ *   ******************
+ *   *                *
+ *   *                *
+ *   ******************
+ */
+
+public class HollowRectangle {
+    public static void main(String args[]) throws Exception{
+
+        int rows , cols;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Rows number : ");
+        rows = scanner.nextInt();
+
+        System.out.print("Cols number : ");
+        cols = scanner.nextInt();
+
+        for ( int i = 1 ; i <= rows ; i++){
+
+            for ( int j = 1 ; j <= cols ; j++) {
+
+                if ( i == 1 || j == 1 || i == rows || j == cols) 
+                    System.out.print("*");
+
+                else 
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}

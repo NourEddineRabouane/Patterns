c18e4a84df3fcccfc530f6c7fa1f9dc9ba77bd66
NourEddineRabouane <rabouanenoureddine1@gmail.com>
2025-11-04 20:16:44 +0100

    Added the right-angled number triangle

--- Cube.java
+++ Cube.java
@@ -3,12 +3,13 @@ package patterns;
 import java.util.Scanner;
 
 public class Cube {
+
     // Define a color to print colored Strings
     public static final String YELLOW = "\u001B[33m";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("side size : ");
         int n = scanner.nextInt();
 
@@ -16,32 +17,25 @@ public class Cube {
         int max = n + space; // the maximun number of prints each line
 
         // Print the first n lines of the cube
-        for (int i = 1; i <= n ; i++) {
+        for (int i = 1; i <= n; i++) {
             int a = space - i + 1; // Calculate the offset for the current line
 
-            for (int j = 1; j <= max ; j++) {
-
-                if ( i <= n ){
-
-                    if (j > a ) System.out.print(YELLOW + " * ");
+            for (int j = 1; j <= max; j++) {
+                if (i <= n) {
+                    if (j > a) System.out.print(YELLOW + " * ");
                     else System.out.print("   ");
-
                 } else {
-                    if ( j <= n + a)
-                        System.out.print(" * ");
-                    else 
-                        System.out.print("  ");
+                    if (j <= n + a) System.out.print(" * ");
+                    else System.out.print("  ");
                 }
-
             }
 
             System.out.println();
         }
-        
+
         // Print the lower part of the cube
-        for (int i = 1 ; i <= space + 1 ; i++){
-            for (int j = max ; j >  i ; j-- )
-                System.out.print(" * ");
+        for (int i = 1; i <= space + 1; i++) {
+            for (int j = max; j > i; j--) System.out.print(" * ");
             System.out.println();
         }
 
--- HalfPyramid.java
+++ HalfPyramid.java
@@ -1,40 +1,39 @@
 package patterns;
+
 import java.util.Scanner;
 
 /*
- *  *
- *  **
- *  ***
- *  ****
+ *  *        ****
+ *  **       ***
+ *  ***      **
+ *  ****     *
  */
 
 public class HalfPyramid {
-    
+
     public static void main(String[] args) {
-        
-        int rows ;
+        int rows;
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("Rows number : ");
         rows = scanner.nextInt();
 
         // Half Pyramid
-        for ( int i = 1 ; i <= rows ; i++ ) {
-            for (int j = 1 ; j <= i ; j++) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
         // Inverted Half Pyramid
-        for ( int i = rows ; i >= 1 ; i-- ) {
-            for (int j = 1 ; j <= i ; j++) {
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
         scanner.close();
-
     }
 }
